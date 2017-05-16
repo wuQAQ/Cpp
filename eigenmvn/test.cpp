6c43986baa26ed2a78e8e9ea0e6847373d65bbae
wuQAQ <302538257@qq.com>
2017-05-17 02:19:04 +0800

    add pca

--- eigenmvn/test.cpp
+++ eigenmvn/test.cpp
@@ -1,16 +1,25 @@
-#include <iostream> 
-#include <cstdio> 
-using namespace std; 
+// reading a text file
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+using namespace std;
+int main ()
+{
+    char buffer[256];
+    ifstream examplefile("test.txt");
+    ofstream exampleoutfile("test-tmp.txt");
+    if (! examplefile.is_open())
+    {
+        cout << "Error opening file"; exit (1);
+    }
+    while (!examplefile.eof())
+    {
+        examplefile.getline(buffer,100);
+        cout<<buffer<< endl;
+        exampleoutfile << buffer << " " << buffer << endl;
+    }
 
-int main() 
-{ 
-  FILE *fp = popen("gnuplot", "w"); 
-  if (fp == NULL) 
-    return -1; 
-  fputs("set mouse\n", fp); 
-  fputs("plot 'samples_solver.txt'\n", fp); 
-  fflush(fp); 
-  cin.get(); 
-  pclose(fp); 
-  return 0; 
-} 
+    exampleoutfile.close();
+    examplefile.close();
+    return 0;
+}
